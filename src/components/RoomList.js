882cfeea2f8c77b1b3fbaba6a03402566ff9d6a9
Ashley Morgan Nowakowski <amn0220@yahoo.com>
2018-04-01 19:55:17 -0700

    checkpoint 3 create rooms: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,21 +6,34 @@ class RoomList extends Component {
       super (props)
 
       this.state = {
-          rooms: []
+          rooms: [],
+          newRoomName: ''
         };
-
+      this.handleChange =this.handleChange.bind(this);
       this.roomsRef = this.props.firebase.database().ref('rooms');
     };
+
 
     componentDidMount() {
      this.roomsRef.on('child_added', snapshot => {
        const room = snapshot.val();
        room.key = snapshot.key;
-       console.log(snapshot.val());
        this.setState({ rooms: this.state.rooms.concat( room ) });
      });
    }
 
+   handleChange(e){
+     this.setState({ newRoomName: e.target.value });
+   }
+
+   handleSubmit(e) {
+      e.preventDefault();
+     alert('A new chat room was created');
+   }
+
+   createChatRoom() {
+        this.roomsRef.push({ name: this.state.newRoomName});
+  }
 
   render() {
       return (
@@ -33,7 +46,16 @@ class RoomList extends Component {
           )
         })}
         </ul>
+        <div className='create-room'>
+          <form className='form' onSubmit={this.handleSubmit}>
+           <fieldset>
+            <legend>Create Chat Room</legend>
+            <input type='text'placeholder='enter chat room name' value={this.state.newRoomName} onChange={(e) => this.handleChange(e)}/>
+            <button type ='submit' onClick={() => this.createChatRoom()}>Add</button>
+           </fieldset>
+          </form>
         </div>
+      </div>
       );
     }
 }
