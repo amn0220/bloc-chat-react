33b44579302b5e62a07bba2d057d26f9c281a7ed
Ashley Morgan Nowakowski <amn0220@yahoo.com>
2018-04-07 23:37:18 -0700

    checkpoint 4/assignmen 4 completed

---
+++ src/components/MessageList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './../App.css';
+
+class MessageList extends Component {
+    constructor (props){
+      super (props)
+
+      this.state = {
+        messages: [{
+          username: '',
+          content: '',
+          sentAt: '',
+          roomId: ''}]
+        };
+      this.messagesRef = this.props.firebase.database().ref('messages');
+      this.state.messages.sentAt = this.props.firebase.database.ServerValue.TIMESTAMP;
+    };
+    componentDidMount() {
+     this.messagesRef.on('child_added', snapshot => {
+       const message = snapshot.val();
+       message.key = snapshot.key;
+       this.setState({ messages: this.state.messages.concat( message ) });
+     });
+    }
+    render(){
+      const activeRoomName = this.props.activeRoomName;
+      const messageList = this.state.messages
+      .filter(message => message.roomId === activeRoomName)
+      .map(message => {
+        return <li className='message' key={message.key}>{message.content}</li>
+      })
+      return (
+      <div className='all-messages'>
+       <ul>{messageList}</ul>
+      </div>
+    );
+    }
+}
+
+export default MessageList;
