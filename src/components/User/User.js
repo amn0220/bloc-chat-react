70165f72178faf755568e87a54e10d35ced94d48
Ashley Morgan Nowakowski <amn0220@yahoo.com>
2018-04-08 14:24:19 -0700

    checkpoint 5 in progress

---
+++ src/components/User/User.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class User extends Component{
+  constructor(props){
+   super (props)
+
+   this.signIn = this.signIn.bind(this);
+   this.signOut = this.signOut.bind(this);
+ }
+
+signIn() {
+  const provider = new this.props.firebase.auth.GoogleAuthProvider();
+  this.props.firebase.auth().signInWithPopup( provider );
+  this.props.setUser();
+}
+
+signOut() {
+  this.props.firebase.auth().signOut();
+  this.props.setUser('Guest');
+}
+
+componentDidMount() {
+this.props.firebase.auth().onAuthStateChanged( user => {
+  this.props.setUser(user);
+  });
+}
+
+render(){
+  return (
+    <div className='welcome-user'>
+      <h1>Hello, {this.props.currentUser}. Welcome to Bloc-Chat!</h1>
+        <button className='sign-in' onClick={() => this.signIn()}> Sign in</button>
+        <button className='sign-out' onClick={() => this.signOut()}> Sign out</button>
+    </div>
+  );
+}
+}
+export default User;
